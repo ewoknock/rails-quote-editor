3649f258662a240b43554f6791e1ec11af6255b5
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-02-12 16:24:37 -0800

    Adds in turbo frames for main pages and some of the functionality

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -11,6 +11,13 @@ class QuotesTest < ApplicationSystemTestCase
     @quote = quotes(:first)
   end
 
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
   test "Creating a new quote" do
     # When we visit the Quotes#index page
     # we expect to see a title with the text "Quotes"
@@ -20,11 +27,9 @@ class QuotesTest < ApplicationSystemTestCase
     # When we click on the link with the text "New Quote"
     #  we expect to land on a page with the title "New Quote"
     click_on "New Quote"
-    assert_selector "h1", text: "New quote"
-
-    # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
     fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
 
     # We expect to be back on the page with the title "Quotes"
@@ -33,21 +38,14 @@ class QuotesTest < ApplicationSystemTestCase
     assert_text "Capybara quote"
   end
 
-  test "Showing a quote" do
-    visit quotes_path
-    click_link @quote.name
-
-    assert_selector "h1", text: @quote.name
-  end
-
   test "Updating a quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
